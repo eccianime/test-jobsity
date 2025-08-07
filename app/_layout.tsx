70ef594c60d53055168551b2ae6f8483fcbf7be5
Jean Paul Rojas <jean.veliz@protiviti.com.br>
2025-08-07 15:37:25 -0300

    feat: add tailwind and fonts configs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import '../global.css';
+
+import {
+  OpenSans_400Regular,
+  OpenSans_600SemiBold,
+  OpenSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/open-sans';
+
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    'open-sans-regular': OpenSans_400Regular,
+    'open-sans-semibold': OpenSans_600SemiBold,
+    'open-sans-bold': OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Image, View } from 'react-native';
+
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+import SplashImage from '../assets/images/splash.png';
 
 export default function Index() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace('/home');
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1'>
+      <Image source={SplashImage} style={{ width: '100%', height: '100%' }} />
     </View>
   );
 }
